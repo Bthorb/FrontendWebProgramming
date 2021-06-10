7506d7eceab9eef4f87b672c240f5718d35da58e
Bthorb <bejapon.ki@rmuti.ac.th>
2021-06-11 00:17:30 +0700

    new

---
+++ js/BasicJs.js
@@ -0,0 +1,12 @@
+document.getElementById('name').innerHTML = " <h5> N </h5>  "
+document.getElementById('name').style = " color: red ; "
+
+document.write('GG')
+console.log('kuy')
+
+alert('OK')
+
+var name = "NN"
+var Name = "N"
+
+console.log("NN"+"N")
