5be8c6b0b882e909392ecfadd83805dfa2f5bc0a
Bthorb <bejapon.ki@rmuti.ac.th>
2021-06-11 16:32:51 +0700

    switch

--- js/BasicJs.js
+++ js/BasicJs.js
@@ -1,12 +1,71 @@
-document.getElementById('name').innerHTML = " <h5> N </h5>  "
-document.getElementById('name').style = " color: red ; "
+// document.getElementById('name').innerHTML = " <h5> N </h5>  "
+// document.getElementById('name').style = " color: red ; "
 
-document.write('GG')
-console.log('kuy')
+// // document.write('GG')
+// // console.log('kuy')
 
-alert('OK')
+// // alert('OK')
 
-var name = "NN"
-var Name = "N"
+// // var name = "NN"
+// // var Name = "N"
 
-console.log("NN"+"N")
+// // console.log("NN"+"N")
+
+// // var num = 12
+// // num += 1
+
+// // console.log(num)
+
+// // var num = 8
+// // let name = "Ben"
+
+// // const PI = 3.1415
+
+// // {
+// //     num = 6.6
+// //     console.log("function :" + num )
+// // }
+// // console.log("without :" + num )
+
+// // const ClickME = () => {
+// //     document.getElementById("button1").innerHTML = "Hiiiiii"
+// // }
+
+// // var arr1 = [2,8]
+// // arr1[1] = 20 
+// // arr1.push(22)
+// // // arr1.pop(1)
+// // arr1.sort( (a, b) => a-b)
+
+// // console.log(arr1)
+
+ var num = 8
+ var n = "B"
+
+// if (num == 1){
+//     num = 2
+// } else{
+//     num = 12
+// }
+
+// if (n == "t"){
+//     n = "m"
+// } else if(n == "k"){
+//     n = "p"
+// } else {
+//     n = "e"
+// }
+
+// switch (num) {
+//     case 8:
+//         num = 5
+//         break;
+
+//     default:
+//         num = 6
+//         break;
+// }
+
+//  console.log(num)
+//  console.log(n)
+
